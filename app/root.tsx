c5c9aa875d5634499fde619a4bd219acb705b199
MaouSanta <40036915+MaouSanta@users.noreply.github.com>
2024-03-29 22:31:52 +0800

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -4,34 +4,35 @@ import '@mantine/carousel/styles.css';
 import {Links, Meta, Outlet, Scripts, ScrollRestoration,} from "@remix-run/react";
 import {ColorSchemeScript, MantineProvider} from "@mantine/core";
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-        <Meta />
-        <Links />
-        <ColorSchemeScript/>
-      </head>
-      <body>
+export function Layout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>MQA</title>
+            <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+            <Meta/>
+            <Links/>
+            <ColorSchemeScript/>
+        </head>
+        <body>
         {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return (
-      <MantineProvider>
-          <Outlet/>
-      </MantineProvider>
-  );
+    return (
+        <MantineProvider>
+            <Outlet/>
+        </MantineProvider>
+    );
 }
 
 export function HydrateFallback() {
-  return <p>Loading...</p>;
+    return <p>Loading...</p>;
 }
