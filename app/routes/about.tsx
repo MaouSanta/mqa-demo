bdcac7f7e34de4f41639b6f12dece0105b6ae8c7
MaouSanta <40036915+MaouSanta@users.noreply.github.com>
2024-04-09 13:23:38 +0800

    Update about.tsx
    Change Video URL

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -97,7 +97,7 @@ export default function About() {
                     </Group>
                     <AspectRatio ratio={16 / 9} miw={'960'}>
                         <iframe width="560" height="315"
-                                src="https://www.youtube.com/embed/okgBstuQSwY?si=UK5ZPnfyo9_5mJ0Q"
+                                src="https://www.youtube.com/embed/xvUuo2ZIqWk?si=QcE-inxfBXyVsTLQ"
                                 title="YouTube video player" frameBorder={0}
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
